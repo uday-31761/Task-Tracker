62a5749c999e0e2e83a5a782990ea040eaee5926
Vura Hari Venkata Naga Jagannadha Uday Kiran <2200031761@kluniversity.in>
2026-06-28 14:18:34 +0530

    Update index.js

--- Server/index.js
+++ Server/index.js
@@ -7,7 +7,15 @@ const TodoModel = require("./Models/Todo");
 
 const app = express();
 
-app.use(cors());
+// app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  })
+);
 app.use(express.json());
 
 // =======================
